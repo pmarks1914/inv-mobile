3a1254b3fa483fbdcb1cff407be5972f79974282
Patrick <pmarks1914@gmail.com>
2024-11-28 00:15:25 +0000

    share file social logo

--- src/Invoice/FileShareButton.js
+++ src/Invoice/FileShareButton.js
@@ -11,7 +11,7 @@ const logo = userDataStore?.user?.file_photo || 'https://test.ventureinnovo.com/
 // get old invoice list
 const getInvoice = JSON.parse(localStorage.getItem("old-invoice"));
 
-console.log("logo >>>>", logo)
+
 const ShareProForma = () => {
   const [isSharing, setIsSharing] = useState(false);
   const [invoiceData, setInvoiceData] = useState(getInvoice);
