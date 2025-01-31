5309d1509b6dc26888841e3d2646e52efa241f2f
Patrick <pmarks1914@gmail.com>
2025-01-31 22:38:52 +0000

    fix name inv

--- src/Invoice/Invoice.js
+++ src/Invoice/Invoice.js
@@ -219,7 +219,7 @@ const InvoiceGenerator = () => {
         </head>
         <body>
 
-        <div class="header" > <strong> ${invoiceData?.invoiceNumber} </strong> </div>
+        <div class="header" > <strong> ${invoiceData?.companyName} </strong> </div>
 
         <!-- <img src=${logo} alt="Company Logo" class="watermark" style="width: 400px; height: auto;" /> -->
 
